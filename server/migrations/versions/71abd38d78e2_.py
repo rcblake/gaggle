c5b92b37e824d4f9cbac17c4685b728b06278bbc
rcblake <127946751+rcblake@users.noreply.github.com>
2023-07-12 16:29:48 -0400

    migrate data

---
+++ server/migrations/versions/71abd38d78e2_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 71abd38d78e2
+Revises: 882d2958b37e
+Create Date: 2023-07-12 14:31:14.620457
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '71abd38d78e2'
+down_revision = '882d2958b37e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ server/migrations/versions/882d2958b37e_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 882d2958b37e
+Revises: d520fa5bec1e
+Create Date: 2023-07-12 12:45:40.176265
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '882d2958b37e'
+down_revision = 'd520fa5bec1e'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
