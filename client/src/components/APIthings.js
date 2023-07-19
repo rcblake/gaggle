7a5db2e32556041f942bef334c4a2b6c77ce1a49
rcblake <127946751+rcblake@users.noreply.github.com>
2023-07-19 09:35:28 -0700

    git ignore update

---
+++ client/src/components/APIthings.js
@@ -0,0 +1,24 @@
+// mapAPI
+
+// url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=PHOTOREF&key=${mapAPI}`
+
+// fucntion intiMap () {
+//     const
+// }
+
+// autocomplete
+// `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&types=(cities)&key=${mapAPI}`
+
+// onChange={handleLocationSearch}
+
+// fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&types=(cities)&key=${mapAPI}`
+// )
+
+// .then(res => {
+//     if (res.ok) {
+//         res.json().then(data => data.map(predict => renderPredict(predict)))
+//             }
+//         }
+//     )
+
+// const renderPredict(predict)
